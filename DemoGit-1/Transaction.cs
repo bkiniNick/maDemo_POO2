9226c94a20bfa0b4d6424be4943977d0e5be7143
Don Nick Munezero <munezerodonnick@gmail.com>
2024-05-27 14:56:46 -0400

    Ajoutez des fichiers projet.

---
+++ DemoGit-1/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGit_1
+{
+    public sealed  class Transaction
+    {
+        public string cardNumber { get; set; }
+        public string owner { get; set; }
+
+        public DateTime ExpirationDate { get; set; }
+
+        public Transaction()
+        { 
+        
+
+
+        }
+
+    }
+}
