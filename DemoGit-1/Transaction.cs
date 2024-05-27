4d15678e2c96fc242ad118c67a5c860c25eca982
Don Nick Munezero <munezerodonnick@gmail.com>
2024-05-27 15:04:05 -0400

    changes on card number's variable type

--- DemoGit-1/Transaction.cs
+++ DemoGit-1/Transaction.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace DemoGit_1
 {
-    public sealed  class Transaction
+    public sealed class Transaction
     {
-        public string cardNumber { get; set; }
+         public DateTime ExpirationDate { get; set; }
+        public int[] cardNumber { get; set; }
         public string owner { get; set; }
 
-        public DateTime ExpirationDate { get; set; }
+       
 
         public Transaction()
         { 
